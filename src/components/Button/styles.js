0c503795e8facb1af0a7e12667c884e055528ca5
Ronielle Leite <ronielleetamy@gmail.com>
2026-03-10 20:16:49 -0300

    Refactor Button styles for consistency and hover effect
    Updated color values to lowercase and adjusted opacity on hover.

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,15 +1,18 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
     padding: 20px;
-    border: 1px solid #CDCDCD;
-    background-color: #00AAF0;
-    color: #FFFFFF;
+    border: 1px solid #cdcdcd;
+    background-color: #00aaf0;
+    color: #ffffff;
     font-size: 24px;
     font-weight: 700;
     flex: 1;
-    
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
     &:hover {
-        opacity: 0.6;
+        opacity: 0.8;
     }
+  
 `
