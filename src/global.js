ca652cde5146236b1ae2907cbb98c4559d89c44b
Ronielle Leite <ronielleetamy@gmail.com>
2026-03-10 20:19:36 -0300

    Refactor global styles export in global.js

--- src/global.js
+++ src/global.js
@@ -1,8 +1,9 @@
-import { createGlobalStyle } from 'styled-components'
+import {createGlobalStyle} from 'styled-components';
 
-export default createGlobalStyle`
-  *, body {
+export const GlobalStyles = createGlobalStyle`
+  * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
   }
 `
